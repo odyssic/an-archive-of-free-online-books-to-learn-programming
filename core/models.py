1a75e1c8e189390f19fc92fd93b6f6619918cf29
Ben Prothe <beprothe@gmail.com>
2020-02-27 14:22:11 -0500

    initial commit. created django files and basic structure

---
+++ core/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = 'tbd'
+    authors = 'tbd'
+    external_link = 'tbd'
+    published_date = 'tbd'
+    write_up = 'tbd'
+    cover_art = 'tbd'
+
+    def __str__(self):
+        return f'{self.title}'
