a101086c6ff2a6acdc2cd04b291a04a4e83c4a73
odyssic <stevenbuddenjr@gmail.com>
2020-02-28 14:20:40 -0500

    created index page showing data

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse, HttpResponseNotFound
+
+from django.shortcuts import get_list_or_404, render, redirect
+
+from .models import Book, Author, Subject
+
+
+def homepage(request):
+    books = Book.objects.all()
+
+    return render(request, 'core/index.html', {'books': books})
