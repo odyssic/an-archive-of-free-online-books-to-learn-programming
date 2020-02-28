c38a76e2fb859cb36fe4a1233fdaa298cbcf1cf2
Ben Prothe <beprothe@gmail.com>
2020-02-28 13:34:49 -0500

    finally database and ForeignKey

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Subject, Author
 
 admin.site.register(Book)
+admin.site.register(Subject)
+admin.site.register(Author)
