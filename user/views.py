0818e0af3c3a788b5cef33ba9dec726b381816fd
Ben Prothe <beprothe@gmail.com>
2020-03-01 14:51:11 -0500

    spruced up me users and tried to connect links

--- user/views.py
+++ user/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
+from django.contrib import messages
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Successfully registered {username}.')
+            return redirect('homepage')
+    else:
+        form = UserCreationForm()
     return render(request, 'user/register.html', {'form': form})
