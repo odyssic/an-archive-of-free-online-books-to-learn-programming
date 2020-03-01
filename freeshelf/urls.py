584942ac26504d38af4730fb90940967d56dfcb2
odyssic <stevenbuddenjr@gmail.com>
2020-02-29 21:55:16 -0500

    working basics of the site

--- freeshelf/urls.py
+++ freeshelf/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage, name='homepage')
+    path('', views.homepage, name='homepage'),
+    path('books/<int:pk>', views.book_detail, name='book-detail'),
+    path('author', views.author, name='author')
+    # static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 ]
