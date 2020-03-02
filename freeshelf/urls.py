a2d6c84ad0f45960b1bd3253dd9ee13a38fe6150
odyssic <stevenbuddenjr@gmail.com>
2020-03-02 15:15:07 -0500

    css working

--- freeshelf/urls.py
+++ freeshelf/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('books/<int:pk>', views.book_detail, name='book-detail'),
     path('author/<int:pk>', views.author, name='author'),
     path('subject/<int:pk>', views.subject, name='subject'),
-    path('image/<int:pk>', views.image, name='image'),
+    # path('image/<int:pk>', views.image, name='image'),
     path('accounts/', include('registration.backends.default.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
