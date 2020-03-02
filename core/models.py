0daa0ae26c30ab75109aeaa62e625b1834c4c70a
Ben Prothe <beprothe@gmail.com>
2020-03-02 11:51:48 -0500

    refined user fields

--- core/models.py
+++ core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 
 class Book(models.Model):
@@ -39,3 +41,5 @@ class Image(models.Model):
 
     def __str__(self):
         return f'{self.imagefile}'
+
+
--- freeshelf/urls.py
+++ freeshelf/urls.py
@@ -1,18 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core import views
-from user import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
-
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
     path('', views.homepage, name='homepage'),
     path('books/<int:pk>', views.book_detail, name='book-detail'),
     path('author/<int:pk>', views.author, name='author'),
     path('subject/<int:pk>', views.subject, name='subject'),
-    path('image/<int:pk>', views.image, name='image')
+    path('image/<int:pk>', views.image, name='image'),
+    path('accounts/', include('registration.backends.default.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
